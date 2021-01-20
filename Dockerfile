a3501bd88d076cc9f768b52bb5b896137d10aafe
Flávio Omar Losada <flavio.losada@senior.com.br>
2021-01-20 17:22:24 -0300

    Adjusts to container with OpenCV and crop image process

--- Dockerfile
+++ Dockerfile
@@ -1,58 +1,14 @@
-FROM public.ecr.aws/lambda/python:3.8
+FROM opencvcourses/opencv:440
 
-# Install OpenCV
+ARG FUNCTION_DIR="/var/task/"
+WORKDIR ${FUNCTION_DIR}
 
-# First: get all the dependencies:
-#
-RUN yum -y update
-RUN yum -y install git libgtk2.0-dev pkg-config libavcodec-dev \
-libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev \
-libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev unzip
-RUN yum -y remove cmake
-RUN yum -y install wget
+RUN pip3 install --target ${FUNCTION_DIR} awslambdaric
 
-# Just get a simple editor for convienience (you could just cancel this line)
-RUN yum -y install vim
-RUN yum -y groupinstall "Development Tools"
-RUN yum -y install tar && yum -y update tar
+ADD aws-lambda-rie /usr/local/bin/aws-lambda-rie
+COPY script.sh requirements.txt app.py ./
 
-RUN wget https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz \
-    && tar -zxvf cmake-3.6.2.tar.gz \
-    && cd cmake-3.6.2 \
-    && ./bootstrap \
-    && make \
-    && make install 
+RUN python3 -m pip install -r ./requirements.txt -t .
 
-# Second: get and build OpenCV 3.2
-#
-RUN cd \
-    && wget https://github.com/opencv/opencv/archive/4.5.0.zip \
-    && unzip 4.5.0.zip \
-    && cd opencv-4.5.0 \
-    && mkdir build \
-    && cd build \
-    && cmake .. \
-    && make -j8 \
-    && make install \
-    && cd \
-    && rm 4.5.0.zip
-
-
-# Third: install and build opencv_contrib
-#
-RUN cd \
-    && wget https://github.com/opencv/opencv_contrib/archive/4.5.0.zip \
-    && unzip 4.5.0.zip \
-    && cd opencv-4.5.0/build \
-    && cmake .. \
-    && make -j8 \
-    && make install \
-    && cd \
-    && rm 4.5.0.zip
-
-COPY app.py requirements.txt ./
-
-RUN python3.8 -m pip install -r requirements.txt -t .
-
-# Command can be overwritten by providing a different command in the template directly.
+ENTRYPOINT [ "/usr/local/bin/python", "-m", "awslambdaric" ]
 CMD ["app.lambda_handler"]
--- app.py
+++ app.py
@@ -3,36 +3,30 @@ import cv2
 import numpy as np
 import io
 import imageio
+import base64
 
+def contour_minor_than_image(contour_area, image_area):
+    p = (contour_area * 100) / image_area
+    return (p <= 90)
 
 def detect_square(image):
-    image = cv2.imdecode(image)
+    gray = cv2.imdecode(image, flags=cv2.IMREAD_GRAYSCALE)
+    image_area = gray.shape[0] * gray.shape[1]
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)    
+    threshold = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]    
+    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
 
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur_image = cv2.medianBlur(gray, 5)
+    biggest_contour = contours[0]
+    biggest_contour_area = cv2.contourArea(biggest_contour)
+    for index in range(1, len(contours)):
+        contour_area = cv2.contourArea(contours[index])
+        if (contour_area > biggest_contour_area and contour_minor_than_image(contour_area, image_area)):
+            biggest_contour = contours[index]
+            biggest_contour_area = contour_area
 
-    sharpen_kernel = np.array([-1, -1, -1], [-1, 9, -1], [-1, -1, -1])
-    sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
-
-    threshold = cv2.threshold(sharpen, 160, 255, cv2.THRESH_BINARY_INV)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    close = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-    contours = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if (len(contours) > 0):
-        contours = contours[0] if len(contours) == 2 else contours[1]
-
-        biggest_contour = contours[0]
-        biggest_contour_area = cv2.contour_area(biggest_contour)
-        for index in range(1, len(contours)):
-            contour_area = cv2.contourArea(contours[index])
-            if (contour_area > biggest_contour_area):
-                biggest_contour = contours[index]
-
-        x, y, w, h = cv2.boundingRect(biggest_contour)
-        ROI = image[y:y+h, x:x+h]
-        return cv2.imencode('.jpg', ROI)
-    raise Exception('No squares found!')
+    x, y, w, h = cv2.boundingRect(biggest_contour)
+    ROI = gray[y:y+h, x:x+w]    
+    return cv2.imencode('.jpg', ROI)
 
 
 def lambda_handler(event, context):
@@ -61,17 +55,19 @@ def lambda_handler(event, context):
 
     image_base64 = body.get('image', '')
 
-    image = imageio.imread(io.BytesIO(base64.b64decode(image_base64)))
+    image_stream = io.BytesIO(base64.b64decode(image_base64))
 
-    cropped_image = detect_square(image)
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
 
-    base64_result = base64.b64encode(cropped_image)
+    cropped_image = detect_square(file_bytes)
+
+    base64_result = base64.b64encode(cropped_image[1])
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "image": base64_result,
+                "image": base64_result.decode('utf-8'),
             }
         ),
     }
